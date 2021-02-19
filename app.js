e9a1e4c44e2f99a183a2f084157c3533795b40ee
Russell <russelldev@protonmail.com>
2021-02-19 15:28:08 +0000

    Add loop to light up squares

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const container = document.getElementById('container');
 const colors = ['#e74c3c', '#8e44ad', '#3498db', 'e67e22', '#2ecc71'];
-const squares = 1000;
+const squares = 2400;
 
+for (let i = 0; i < squares; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    container.appendChild(square);
+    square.addEventListener('mouseover', () => {setColor(square)});
+    square.addEventListener('mousleave', () => {setColor(square)});
+    container.appendChild(square);
+}
