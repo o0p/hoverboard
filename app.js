ac5ce8c54e26bf7744e97ba77ee55a0e6120c8d7
Russell <russelldev@protonmail.com>
2021-02-19 15:58:29 +0000

    Add color remover

--- app.js
+++ app.js
@@ -17,20 +17,20 @@ for (let i = 0; i < squares; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
     container.appendChild(square);
-    square.addEventListener('mouseover', () => {setColor(square)});
-    square.addEventListener('mousleave', () => {removeColor(square)});
+    square.addEventListener('mouseover', () => setColor(square));
+    square.addEventListener('mouseout', () => removeColor(square));
     container.appendChild(square);
 }
 
 function setColor(element) {
     const color = getRandomColor();
-
     element.style.background = color;
     element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 };
 
 function removeColor(element) {
-
+    element.style.background = '#1d1d1d'
+    element.style.boxShadow = '0 0 2px #000'
 };
 
 function getRandomColor() {
